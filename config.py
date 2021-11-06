a106ff8d7f9a642cd085492f805b9e60faba26d9
alcyada <89109873+alcyada@users.noreply.github.com>
2021-11-06 21:03:23 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+from os import getenv
+BOT_TOKEN = getenv("BOT_TOKEN", "1524532914:AAFoX2h5yfr9QPJSrdsA0nhozS-GDfU1TWI")
+API_ID = int(getenv("API_ID", "2734456"))
+API_HASH = getenv("API_HASH", "74cb6a90caf6fa6ccd801ae6bf83005c")

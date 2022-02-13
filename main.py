b4f0b92c350cde8da50765c66a7a86b876c88fae
alcyada <89109873+alcyada@users.noreply.github.com>
2022-02-13 19:24:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import requests
-from pyrogram import Client as Bot
-from pyrogram import idle
-from config import API_ID, API_HASH, BOT_TOKEN
+#import requests
+#from pyrogram import Client as Bot
+#from pyrogram import idle
+#from config import API_ID, API_HASH, BOT_TOKEN
 
 
-bot = Bot(
-    ":memory:",
-    API_ID,
-    API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="plugins")
-)
+#bot = Bot(
+ #   ":memory:",
+ #   API_ID,
+#API_HASH,
+#    bot_token=BOT_TOKEN,
+#   plugins=dict(root="plugins")
+#)
 
-bot.start()
+#bot.start()
 
-idle()
+#idle()
